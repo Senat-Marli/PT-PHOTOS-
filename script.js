f49fa7a7297b306185096a0d8898e416b2822440
Senat-Marli <tursunovigorexa103@gmail.com>
2026-08-10 19:51:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -65,5 +65,4 @@ document.addEventListener('DOMContentLoaded', function() {
         const today = new Date().toISOString().split('T')[0];
         weddingDate.setAttribute('min', today);
     }
-
 });
